5085cb0a52446e3d9bae75cb3a8469ae280fc997
DanWeissman <dan.bayford@weissman.agency>
2024-09-29 20:06:56 +0100

    add auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+/*
+Anything request starting 'api/auth/' will be considered handled by nextauth 
+(ie it will be considered api/auth/nextauth/route.ts)
+*/
+import NextAuth, { CallbacksOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+// Exposes GET and POST routes
+export { handler as GET, handler as POST };
---
+++ src/components/SessionProvider.tsx
@@ -0,0 +1,6 @@
+"use client";
+/* 
+Wrapping all the next-auth exports inside a client component file marks it as client components 
+It can now be used in the root layout
+*/
+export * from "next-auth/react";
